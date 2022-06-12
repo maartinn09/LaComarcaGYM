0ef2eee66d62f754b96f0c59fd58817ca01869a2
maartinn09 <91863299+maartinn09@users.noreply.github.com>
2022-06-12 20:23:41 -0300

    Add files via upload

---
+++ js1/main.js
@@ -0,0 +1,9 @@
+import slider from "./slider.js";
+import contactFormValidations from "./validaciones.js";
+
+const d = document;
+
+d.addEventListener("DOMContentLoaded", (e)=>{
+    slider();
+    contactFormValidations();
+});
